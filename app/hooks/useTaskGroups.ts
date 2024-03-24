ad3fcf0239a2e7e48a69950dcbc34f53c270b1c9
Tural Hajiyev <thajiyev9@outlook.com>
2024-03-25 00:20:04 +0100

    Removed unnecessary abstraction

--- app/hooks/useTaskGroups.ts
+++ app/hooks/useTaskGroups.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
-import { API_ENDPOINT } from '../utils/api';
 import { TaskGroupType } from '../utils/types';
+
+/* API endpoint to fetch task groups */
+const API_ENDPOINT: string =
+  'https://gist.githubusercontent.com/huvber/ba0d534f68e34f1be86d7fe7eff92c96/raw/98a91477905ea518222a6d88dd8b475328a632d3/mock-progress';
 
 /**
  * Custom hook for managing task groups.
--- app/utils/api.ts
+++
@@ -1,4 +0,0 @@
-const API_ENDPOINT: string =
-  'https://gist.githubusercontent.com/huvber/ba0d534f68e34f1be86d7fe7eff92c96/raw/98a91477905ea518222a6d88dd8b475328a632d3/mock-progress';
-
-export { API_ENDPOINT };

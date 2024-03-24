3d2277cf4fc2fab5c139bc2a12cd78ad5f37d770
Tural Hajiyev <thajiyev9@outlook.com>
2024-03-25 00:13:20 +0100

    Added comments and docstrings

--- app/utils/tasks.ts
+++ app/utils/tasks.ts
@@ -1,19 +1,25 @@
 import { TaskGroupType } from './types';
 
+/**
+ * Calculates the normalized sum based on the checked tasks within the task groups.
+ */
 export const calculateNormalizedSum = (taskGroups: TaskGroupType[]): number => {
-  let totalValue = 0;
-  let checkedValue = 0;
-
+  let totalValue = 0; // Total value of all tasks
+  let checkedValue = 0; // Total value of checked tasks
   taskGroups.forEach(group => {
+    // Summing up the values of tasks within each group
     totalValue += group.tasks.reduce((acc, task) => {
       if (task.checked) {
+        // If the task is checked, increment checked value by the task's value
         checkedValue += task.value;
       }
+      // Accumulate task value
       return acc + task.value;
     }, 0);
   });
-
-  const normalizedSum = totalValue !== 0 ? (checkedValue * 100) / totalValue : 0;
-
-  return Number(normalizedSum.toFixed(2)); 
+  // Calculating the normalized sum
+  const normalizedSum =
+    totalValue !== 0 ? (checkedValue * 100) / totalValue : 0;
+  // Returning the normalized sum rounded to 2 decimal places
+  return Number(normalizedSum.toFixed(2));
 };

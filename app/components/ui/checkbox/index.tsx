95db80315a02fdcfa35eb9712e657d359160c81f
Tural Hajiyev <thajiyev9@outlook.com>
2024-03-25 00:19:02 +0100

    More comments and updated wrong prop names

--- app/components/ui/checkbox/index.tsx
+++ app/components/ui/checkbox/index.tsx
@@ -38,8 +38,8 @@ const Checkbox: React.FC<CheckboxProps> = ({
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M3.49574 6.10067L0.888005 3.30201L0 4.24832L3.49574 8L10 0.946309L9.11825 0L3.49574 6.10067Z"
           fill="currentColor"
         />
--- app/hooks/useTaskGroups.ts
+++ app/hooks/useTaskGroups.ts
@@ -2,21 +2,33 @@ import { useEffect, useState } from 'react';
 import { API_ENDPOINT } from '../utils/api';
 import { TaskGroupType } from '../utils/types';
 
+/**
+ * Custom hook for managing task groups.
+ */
 export const useTaskGroups = () => {
-  /* Fetching state. Regardless of failure or not, after the first state */
   const [taskGroups, setTaskGroups] = useState<TaskGroupType[]>([]);
 
+  /**
+   * Fetch task groups from API and store in state.
+   */
   async function fetchTaskGroups() {
     try {
       const response = await fetch(API_ENDPOINT);
-      const taskGroups = await response.json();
-      setTaskGroups(taskGroups);
+      if (response.ok) {
+        const taskGroups = await response.json();
+        setTaskGroups(taskGroups);
+      } else {
+        throw new Error(await response.text());
+      }
       return taskGroups;
     } catch (error) {
       console.error('Error fetching task groups:', error);
     }
   }
 
+  /**
+   * Toggle the checked state of a task.
+   */
   function toggleTaskChecked(groupIndex: number, taskIndex: number) {
     setTaskGroups(prevGroups => {
       const newTaskGroups = [...prevGroups];
@@ -30,10 +42,12 @@ export const useTaskGroups = () => {
     });
   }
 
+  // Fetch task groups on initial render
   useEffect(() => {
     fetchTaskGroups();
   }, []);
 
+  // Return taskGroups state and toggleTaskChecked function
   return {
     taskGroups,
     toggleTaskChecked,

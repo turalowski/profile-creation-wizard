b453125ef3db5678b89dc29ce4e2cb63bf4fab78
Tural Hajiyev <thajiyev9@outlook.com>
2024-03-25 02:24:11 +0100

    refactored calculateNormalizedSum function to avoid unnecessary argument passing

--- app/containers/profile-creation/index.tsx
+++ app/containers/profile-creation/index.tsx
@@ -4,11 +4,17 @@ import { useTaskGroups } from '@/app/hooks/useTaskGroups';
 import { calculateNormalizedSum } from '@/app/utils/tasks';
 import { TaskGroups } from './task-groups';
 import { Header } from './haeder';
+import { TaskGroupType, TaskType } from '@/app/utils/types';
 
 /* Component to show User creation wizard */
 export const ProfileCreation: React.FC = () => {
   const { taskGroups, toggleTaskChecked } = useTaskGroups();
-  const progress: number = calculateNormalizedSum(taskGroups);
+  /* combine tasks from multiple groups to calculate progress */
+  const combinedTasks: TaskType[] = taskGroups.reduce((acc: TaskType[], curr: TaskGroupType) => {
+    return acc.concat(curr.tasks);
+}, []);
+  /* calculate completed percentage */
+  const progress: number = calculateNormalizedSum(combinedTasks);
   return (
     <section className="border p-4 rounded-lg w-full">
       <Header progress={progress} />
--- app/utils/tasks.ts
+++ app/utils/tasks.ts
@@ -1,22 +1,22 @@
-import { TaskGroupType } from './types';
+import { TaskType } from './types';
 
 /**
- * Calculates the normalized sum based on the checked tasks within the task groups.
+ * Calculates the normalized sum based on the checked tasks
  */
-export const calculateNormalizedSum = (taskGroups: TaskGroupType[]): number => {
+export const calculateNormalizedSum = (
+  tasks: Omit<TaskType, 'description'>[]
+): number => {
   let totalValue = 0; // Total value of all tasks
   let checkedValue = 0; // Total value of checked tasks
-  taskGroups.forEach(group => {
-    // Summing up the values of tasks within each group
-    totalValue += group.tasks.reduce((acc, task) => {
-      if (task.checked) {
-        // If the task is checked, increment checked value by the task's value
-        checkedValue += task.value;
-      }
-      // Accumulate task value
-      return acc + task.value;
-    }, 0);
-  });
+  // Summing up the values of tasks within each group
+  totalValue += tasks.reduce((acc, task) => {
+    if (task.checked) {
+      // If the task is checked, increment checked value by the task's value
+      checkedValue += task.value;
+    }
+    // Accumulate task value
+    return acc + task.value;
+  }, 0);
   // Calculating the normalized sum
   const normalizedSum =
     totalValue !== 0 ? (checkedValue * 100) / totalValue : 0;
